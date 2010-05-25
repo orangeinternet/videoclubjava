5024ef005d6a9f0a1948a47ef7bca64510ef5a74
bajomurdock <bajomurdock@37313fdf-3c53-605c-126e-db64c1c7843d>
2010-05-25 11:19:56 +0000

    version con comentarios

--- src/videoclub/Oficina.java
+++ src/videoclub/Oficina.java
@@ -7,11 +7,17 @@ public class Oficina {
 	/*
 	 * a continuacion vamos a declarar los atributos de la clase Oficina.
 	 */
+	/**
+	 * atributos de la clase Oficina
+	 */
 	private int telefono;
 	private String direccion;
 	private String responsable;
 
 	// creamos los setters and getters
+	/**
+	 * get y set del atributo telefono.
+	 */
 	public int getTelefono() {
 		return telefono;
 	}
@@ -19,7 +25,10 @@ public class Oficina {
 	public void setTelefono(int telefono) {
 		this.telefono = telefono;
 	}
-
+	/**
+	 * get y set del atributo responsable.
+	 * @return
+	 */
 	public String getResponsable() {
 		return responsable;
 	}
@@ -27,7 +36,10 @@ public class Oficina {
 	public void setResponsable(String responsable) {
 		this.responsable = responsable;
 	}
-
+	/**
+	 * get y set del atributo direccion.
+	 * @return
+	 */
 	public String getDireccion() {
 		return direccion;
 	}
@@ -37,12 +49,17 @@ public class Oficina {
 	}
 
 	// el constructor Oficina
+	/**
+	 * para construir un nuevo objeto de Oficina
+	 */
 	public Oficina() {
 
 	}
 
 	// un metodo toString que nos devuelve los traibutos de la clase
-	
+	/**
+	 * llamamos al metodo toString.
+	 */
 	public String toString() {
 		return ("La Clase Oficina: " + responsable + direccion + telefono);
 
