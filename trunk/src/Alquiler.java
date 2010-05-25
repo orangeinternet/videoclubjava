1b313b5c189b180de1a9285ff17f720c51d82864
jairofigueroajimenez <jairofigueroajimenez@37313fdf-3c53-605c-126e-db64c1c7843d>
2010-05-25 07:01:08 +0000

    Creacion de la clase alquiler

---
+++ trunk/src/Alquiler.java
@@ -0,0 +1,103 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+	/**
+	 * @author Jairo Figueroa Jimenez
+	 * @version 1.0
+	 * Fecha: 25/05/2010 
+	 */
+
+public class Alquiler {
+	
+	// Atributos de la clase
+	
+	private Socio socio;
+	private Pelicula pelicula;
+	private Date fechaAlquiler;
+	
+	// Constructor
+	
+	Alquiler(){}
+
+	// Getters y Setters de la clase Alquiler
+	
+	public Socio getSocio() {
+		return socio;
+	}
+
+	public void setSocio(Socio socio) {
+		this.socio = socio;
+	}
+
+	public Pelicula getPelicula() {
+		return pelicula;
+	}
+
+	public void setPelicula(Pelicula pelicula) {
+		this.pelicula = pelicula;
+	}
+
+	public Date getFechaAlquiler() {
+		return fechaAlquiler;
+	}
+
+	public void setFechaAlquiler(Date fechaAlquiler) {
+		this.fechaAlquiler = fechaAlquiler;
+	}
+	
+	/**
+	 * Metodo toString sobre-escrito de la clase object
+	 * que devuelve una cadena
+	 * 
+	 * return String
+	 */
+	
+	public String toString(){
+		
+	}
+	
+	/**
+	 * La clave de todo está en el método setLenient. Si se establece a false, como se hace en el método 
+	 * se fuerza a que la fecha "tenga sentido estricto",
+	 * y por lo tanto rechaza un "30 de febrero" o un "29 de febrero de 2007" como fechas válidas.
+       Si no establecemos el lenient a false, al parsear una fecha "interpretará" la fecha correcta.
+       Un "30 de febrero" se convertirá en 1 marzo, (en 2 de marzo si es un año no bisiesto)...
+	 * 
+	 * @param fechax
+	 * @return
+	 */
+	
+	public static boolean isFechaValida(String fechax) {
+		try {
+			SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy",
+			Locale.getDefault());
+			formatoFecha.setLenient(false);
+			formatoFecha.parse(fechax);
+		} catch (ParseException e) {
+			return 
+				false;
+		}
+
+		return true;
+	} 
+	
+	/**
+	 * Funcion que pasea un string a un Date
+	 * 
+	 * return Date
+	 */
+	
+	public Date StringAdate(String fecha) {
+		SimpleDateFormat formato= new SimpleDateFormat("dd-MM-yyyy");
+		Date d = null;
+		try {
+			d = formato.parse(fecha);
+		} catch (ParseException e) {
+			System.out.println("Error al parsear la fecha: " + e.toString());
+		} finally{
+			return d;
+		}
+	}
+}
